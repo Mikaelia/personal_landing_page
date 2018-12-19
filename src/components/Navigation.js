06f774152ee0612ef8cb06087c5a72c8832d96a1
Mikaela Gurney <354@holbertonschool.com>
2018-12-18 19:47:18 -0800

    Fix Navigation close on click

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,64 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function Navigation(props) {
-  return (
-    <div className="navigation">
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
-      />
+class Navigation extends Component {
+  state = {
+    checked: false
+  };
+  handleCheck = () => {
+    this.setState({ checked: !this.state.checked });
+  };
+  render() {
+    return (
+      <div className="navigation">
+        <input
+          type="checkbox"
+          className="navigation__checkbox"
+          id="navi-toggle"
+          checked={this.state.checked}
+          onChange={this.handleCheck}
+        />
 
-      <label for="navi-toggle" className="navigation__button">
-        <span className={"navigation__icon" + props.modifier}>&nbsp;</span>
-      </label>
+        <label htmlFor="navi-toggle" className="navigation__button">
+          <span className={"navigation__icon" + this.props.modifier}>
+            &nbsp;
+          </span>
+        </label>
 
-      <div className="navigation__background">&nbsp;</div>
+        <div className="navigation__background">&nbsp;</div>
 
-      <nav className="navigation__nav">
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <Link to="/" className="navigation__link">
-              <span>01</span>Home
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <a href="https://github.com/Mikaelia" className="navigation__link">
-              <span>02</span>Projects
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a
-              href="https://www.facebook.com/mikaelaspencils/"
-              className="navigation__link"
-            >
-              <span>03</span>Pencil Art
-            </a>
-          </li>
-          <li className="navigation__item">
-            <Link to="/resume" className="navigation__link">
-              <span>04</span>Resume
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <a
-              href="https://www.linkedin.com/in/mikaela-gurney-947926b9/"
-              className="navigation__link"
-            >
-              <span>05</span>LinkedIn
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a
-              href="https://twitter.com/MikaelaGurney"
-              className="navigation__link"
-            >
-              <span>05</span>Social
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+        <nav className="navigation__nav">
+          <ul className="navigation__list">
+            <li className="navigation__item" onClick={this.handleCheck}>
+              <Link to="/" className="navigation__link">
+                <span>01</span>Home
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="https://github.com/Mikaelia"
+                className="navigation__link"
+              >
+                <span>02</span>Projects
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="https://www.facebook.com/mikaelaspencils/"
+                className="navigation__link"
+              >
+                <span>03</span>Pencil Art
+              </a>
+            </li>
+            <li className="navigation__item" onClick={this.handleCheck}>
+              <Link to="/resume" className="navigation__link">
+                <span>04</span>Resume
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="https://www.linkedin.com/in/mikaela-gurney-947926b9/"
+                className="navigation__link"
+              >
+                <span>05</span>LinkedIn
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="https://twitter.com/MikaelaGurney"
+                className="navigation__link"
+              >
+                <span>05</span>Social
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
+
+export default Navigation;
--- src/components/ResumePage/ResumePage.js
+++ src/components/ResumePage/ResumePage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PDFViewer from "./PDFViewer";
 import PDFJSBackend from "./backends/pdfs.js";
 
