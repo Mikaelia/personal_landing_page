d52c724f789d12f99ceb6eb544b9596e91fb7bab
Mikaela Gurney <354@holbertonschool.com>
2018-12-21 14:46:56 -0800

    Add project links

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import colorCollector from "../static/colorCollector.png";
+import { Link } from "react-router-dom";
 
 const ProjectView = () => {
   return (
@@ -8,22 +8,54 @@ const ProjectView = () => {
       <div className="project__card-container">
         <div className="project__card">
           <span className="project__card-name">TrailFinder</span>
+          <a
+            className="project__link"
+            href="https://github.com/Mikaelia/TrailFinder"
+          >
+            To Project &rarr;
+          </a>
           <div className="project__card-image card-image--1" />
         </div>
         <div className="project__card">
           <span className="project__card-name">Color Collector</span>
+          <a
+            className="project__link"
+            href="https://github.com/Mikaelia/color-collector"
+          >
+            To Project &rarr;
+          </a>
+
           <div className="project__card-image card-image--2" />
         </div>
         <div className="project__card">
           <span className="project__card-name">C Shell</span>
+          <a
+            className="project__link"
+            href="https://github.com/Mikaelia/C_shell"
+          >
+            To Project &rarr;
+          </a>
           <div className="project__card-image card-image--3" />
         </div>
         <div className="project__card">
-          <span className="project__card-name">AirBnB</span>
+          <span className="project__card-name">AirBnB Clone</span>
+          <a
+            className="project__link"
+            href="https://github.com/Mikaelia/AirBnB_clone_v4"
+          >
+            To Project &rarr;
+          </a>
           <div className="project__card-image card-image--4" />
         </div>
         <div className="project__card">
           <span className="project__card-name"> HackerNews</span>
+          <a
+            className="project__link"
+            href="https://github.com/Mikaelia/hackernews-manager"
+          >
+            To Project &rarr;
+          </a>
+
           <div className="project__card-image card-image--5">
             * Coming Soon *
           </div>
