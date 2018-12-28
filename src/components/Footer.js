2fde3bee92a951bc803772fb22d5c98c09774a06
Mikaela Gurney <354@holbertonschool.com>
2018-12-27 22:49:09 -0800

    Adjust project section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Tree from "./Tree";
 
 export default function Footer() {
   return (
     <div id="footer" className="footer">
+      <Tree />
       FOOTER
     </div>
   );
